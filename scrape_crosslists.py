98e5d6e9ab19c0e4b351eaba0be572032fabd857
Sahan Kumarasinghe <sahansk2@illinois.edu>
2021-07-04 00:02:16 -0400

    Updated how crosslist data was exported

--- scrape_crosslists.py
+++ scrape_crosslists.py
@@ -2,7 +2,10 @@ import uiuc_api as ua
 from util.export import OutputCSV
 from util.extractor import extract_crosslist_from_description as get_crosslist
 
-with open('./data/deptCodes.txt', 'r') as codeFile:
+with OutputCSV('crosslist.csv', [
+            'courseDept', 'courseNum', 'targetDept', 'targetNum'
+        ]) as out, open('./data/deptCodes.txt', 'r') as codeFile:
+    out.write_header()
     for dept in codeFile:
         dept = dept.strip()
         try:
@@ -10,18 +13,15 @@ with open('./data/deptCodes.txt', 'r') as codeFile:
         except ValueError:
             print(f"Couldn't get information about department {dept}! Skipping...")
             continue
-        with OutputCSV('crosslist.csv', [
-            'courseDept', 'courseNum', 'targetDept', 'targetNum'
-        ]) as out:
-            for course in ua.get_courses(catalog):
-                try:
-                    xlist = get_crosslist(course.description)
-                    if xlist:
-                        out.insert(
-                            targetDept=xlist[0],
-                            targetNum=xlist[1],
-                            courseDept=course.subject,
-                            courseNum=course.number
-                        )
-                except TypeError as e:
-                    print("Got TypeError! Check course: {} {}".format(course.subject, course.number))
+        for course in ua.get_courses(catalog):
+            try:
+                xlist = get_crosslist(course.description)
+                if xlist:
+                    out.insert(
+                        targetDept=xlist[0],
+                        targetNum=xlist[1],
+                        courseDept=course.subject,
+                        courseNum=course.number
+                    )
+            except TypeError as e:
+                print("Got TypeError! Check course: {} {}".format(course.subject, course.number))
